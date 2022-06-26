fc0576b25ef27c8177f2ca328b5642aee87d9938
EA-Emerson <99118774+EA-Emerson@users.noreply.github.com>
2022-06-26 14:40:02 +0100

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,28 @@
+let input = document.querySelector('.input')
+let btns = document.querySelectorAll('button')
+let submit = document.querySelector('.submit')
+// let del = document.querySelector('.DEL')
+
+btns.forEach(function(btn){
+
+    btn.addEventListener('click', function(e){
+
+    var x=e.target.innerHTML
+
+    input.value += x
+
+    let styles=input.value
+
+    if (styles.includes('AC')) {
+        input.value=this.nonce
+    }
+        submit.addEventListener('click', function(){
+            input.value=eval(styles);  
+            return   
+        })
+
+    
+
+})
+
+})
